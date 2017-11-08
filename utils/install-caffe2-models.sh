f77706826658626df5f8da6a74eada0854dde96b
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2017-11-08 14:33:17 +0800

    Install squeezenet model for caffe2.
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

---
+++ utils/install-caffe2-models.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Main dependencies
+
+sudo apt-get update
+sudo apt-get install -y git-lfs
+
+# Download models
+
+TMPDIR1=`mktemp -d`
+cd "$TMPDIR1"
+git lfs clone https://github.com/caffe2/models.git
+
+# Install models
+
+sudo mkdir -p /caffe2/caffe2/python/models
+sudo cp -f models/squeezenet/*.pb /caffe2/caffe2/python/models
+sudo cp -f models/bvlc_alexnet/*.npy /caffe2/caffe2/python/models
