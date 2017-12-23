975405c58b389d28278c67b5fa017a0899b7df4d
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2017-12-23 14:55:09 +0800

    caffe2: Fix FTBFS on Raspbian.
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

---
+++ utils/install-caffe2-raspbian.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Main dependencies
+
+sudo apt-get update
+sudo apt-get install -y \
+      build-essential \
+      git \
+      cmake \
+      libgflags-dev \
+      libgoogle-glog-dev \
+      libprotobuf-dev \
+      libpython-dev \
+      python-pip \
+      python-numpy \
+      protobuf-compiler \
+      python-protobuf
+
+# Build and install caffe2
+
+cd /   # Not sure why we need to build from /
+sudo git clone --recursive https://github.com/caffe2/caffe2.git
+cd caffe2
+sudo ./scripts/build_raspbian.sh
+cd build
+sudo make install
