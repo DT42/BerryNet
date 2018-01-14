ab6c3b3f7ec44ca1067c8f28dda8f0f7b02fb1ea
Bofu Chen (bafu) <bofu@dt42.io>
2018-01-14 14:10:30 +0800

    Download Caffe2 squeezenet model file by wget instead of download.py.
    Signed-off-by: Bofu Chen (bafu) <bofu@dt42.io>

--- utils/install-caffe2-models.sh
+++ utils/install-caffe2-models.sh
@@ -6,9 +6,7 @@ sudo apt-get update
 #sudo apt-get install -y git-lfs
 
 # Download models
-
-CAFFE2_MODEL_DIR="/caffe2/caffe2/python/models"
-
+#
 # FIXME: caffe2.python.models.download can not download correct
 #   squeezenet model files currently.
 #
@@ -20,6 +18,8 @@ CAFFE2_MODEL_DIR="/caffe2/caffe2/python/models"
 #
 # Caffe2 model repo: https://github.com/caffe2/models/tree/master/squeezenet
 
+CAFFE2_MODEL_DIR="/caffe2/caffe2/python/models"
+
 #TMPDIR1=`mktemp -d`
 #cd /caffe2/build
 #sudo python -m caffe2.python.models.download squeezenet
@@ -30,11 +30,13 @@ CAFFE2_MODEL_DIR="/caffe2/caffe2/python/models"
 
 
 # Install models
+#
+# FIXME: git-lfs is unavailable on Raspbian.
 
-sudo mkdir -p $CAFFE2_MODEL_DIR
-git clone https://github.com/caffe2/models.git /tmp/caffe2-models
-sudo cp -a /tmp/caffe2-models/squeezenet $CAFFE2_MODEL_DIR
-sudo cp -a /tmp/caffe2-models/bvlc_alexnet $CAFFE2_MODEL_DIR
-rm -rf /tmp/caffe2-models
+sudo mkdir -p $CAFFE2_MODEL_DIR/squeezenet
+sudo wget -O $CAFFE2_MODEL_DIR/squeezenet/exec_net.pb \
+    https://github.com/caffe2/models/raw/master/squeezenet/exec_net.pb
+sudo wget -O $CAFFE2_MODEL_DIR/squeezenet/predict_net.pb \
+    https://github.com/caffe2/models/raw/master/squeezenet/predict_net.pb
 #sudo cp -f models/squeezenet/*.pb /caffe2/caffe2/python/models
 #sudo cp -f models/bvlc_alexnet/*.npy /caffe2/caffe2/python/models
