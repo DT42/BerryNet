189ba9607ab1c0488d8f6d137fe786d2a11457f5
Bofu Chen (bafu) <bofu@dt42.io>
2018-02-19 18:34:32 +0800

    Add generic [de]serialize_payload function in payload module.
    Signed-off-by: Bofu Chen (bafu) <bofu@dt42.io>

--- berrynet/comm/payload.py
+++ berrynet/comm/payload.py
@@ -11,12 +11,12 @@ def stringify_jpg(jpg_bytes):
     return base64.b64encode(jpg_bytes).decode('utf-8')
 
 
-def destringify_jpg(serialized_jpg):
+def destringify_jpg(stringified_jpg):
     """
     :return: JPEG bytes
     :rtype: bytes
     """
-    return base64.b64decode(serialized_jpg.encode('utf-8'))
+    return base64.b64decode(stringified_jpg.encode('utf-8'))
 
 
 def jpg2bgr(jpg_bytes):
@@ -36,6 +36,10 @@ def jpg2rgb(jpg_bytes):
     return cv2.cvtColor(jpg2bgr(jpg_bytes), cv2.COLOR_BGR2RGB)
 
 
+def serialize_payload(json_object):
+    return json.dumps(json_object)
+
+
 def serialize_jpg(jpg_bytes):
     """Create Serialized JSON object consisting of image bytes and meta
 
@@ -50,14 +54,18 @@ def serialize_jpg(jpg_bytes):
     return json.dumps(obj)
 
 
-def deserialize_jpg(jpg_json):
-    """Deserialized JSON object created by josnify_image.
+def deserialize_payload(payload):
+    return json.loads(payload)
 
-    :param string :
-    :return:
-    :rtype:
-    """
-    return json.loads(jpg_json)
+
+#def deserialize_jpg(jpg_json):
+#    """Deserialized JSON object created by josnify_image.
+#
+#    :param string :
+#    :return:
+#    :rtype:
+#    """
+#    return json.loads(jpg_json)
 
 
 if __name__ == '__main__':
@@ -66,7 +74,7 @@ if __name__ == '__main__':
 
     # size of stringified dog.jpg is 1.33x larger than original
     s_jpg = serialize_jpg(jpg_bytes)
-    d_jpg = deserialize_jpg(s_jpg)
+    d_jpg = deserialize_payload(s_jpg)
     # TODO: Can we write JPEG bytes into file directly to prevent
     #       bytes -> numpy array -> decode RGB -> write encoded JPEG
     cv2.imwrite('/tmp/dog.jpg', jpg2bgr(destringify_jpg(d_jpg['bytes'])))
