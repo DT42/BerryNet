f20a7112c79fe1a7b1f9b257338153194f8ef769
Bofu Chen (bafu) <bofu@dt42.io>
2018-03-01 20:29:39 +0800

    Add mockup service for testing.
    Signed-off-by: Bofu Chen (bafu) <bofu@dt42.io>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 
 setup(
     name='berrynet',
-    version='v3.0.0rc1',
+    version='v3.0.0rc2',
     description='BerryNet',
     long_description=
         'TBD',
