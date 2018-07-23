0af30c46d767089a05f22ed2b5a233907af6f39d
Bofu Chen (bafu) <bafuchen@gmail.com>
2018-07-23 20:39:19 +0800

    Add Darknet Python engine and service.
    Signed-off-by: Bofu Chen (bafu) <bafuchen@gmail.com>

---
+++ berrynet/engine/darknet_engine.py
@@ -0,0 +1,188 @@
+# Copyright 2018 DT42
+#
+# This file is part of BerryNet.
+#
+# BerryNet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BerryNet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BerryNet.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Darknet inference engine.
+"""
+
+from __future__ import print_function
+
+import argparse
+import json
+import math
+import time
+
+import cv2
+import numpy as np
+
+from berrynet import logger
+#from berrynet.dlmodelmgr import DLModelManager
+from berrynet.engine import DLEngine
+from ctypes import *
+
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+
+class METADATA(Structure):
+    _fields_ = [("classes", c_int),
+                ("names", POINTER(c_char_p))]
+
+
+lib = CDLL("/usr/lib/libdarknet.so", RTLD_GLOBAL)
+lib.network_width.argtypes = [c_void_p]
+lib.network_width.restype = c_int
+lib.network_height.argtypes = [c_void_p]
+lib.network_height.restype = c_int
+
+predict = lib.network_predict
+predict.argtypes = [c_void_p, POINTER(c_float)]
+predict.restype = POINTER(c_float)
+
+make_image = lib.make_image
+make_image.argtypes = [c_int, c_int, c_int]
+make_image.restype = IMAGE
+
+make_boxes = lib.make_boxes
+make_boxes.argtypes = [c_void_p]
+make_boxes.restype = POINTER(BOX)
+
+free_ptrs = lib.free_ptrs
+free_ptrs.argtypes = [POINTER(c_void_p), c_int]
+
+num_boxes = lib.num_boxes
+num_boxes.argtypes = [c_void_p]
+num_boxes.restype = c_int
+
+make_probs = lib.make_probs
+make_probs.argtypes = [c_void_p]
+make_probs.restype = POINTER(POINTER(c_float))
+
+reset_rnn = lib.reset_rnn
+reset_rnn.argtypes = [c_void_p]
+
+load_net = lib.load_network
+load_net.argtypes = [c_char_p, c_char_p, c_int]
+load_net.restype = c_void_p
+
+free_image = lib.free_image
+free_image.argtypes = [IMAGE]
+
+letterbox_image = lib.letterbox_image
+letterbox_image.argtypes = [IMAGE, c_int, c_int]
+letterbox_image.restype = IMAGE
+
+load_meta = lib.get_metadata
+lib.get_metadata.argtypes = [c_char_p]
+lib.get_metadata.restype = METADATA
+
+load_image = lib.load_image_color
+load_image.argtypes = [c_char_p, c_int, c_int]
+load_image.restype = IMAGE
+
+rgbgr_image = lib.rgbgr_image
+rgbgr_image.argtypes = [IMAGE]
+
+predict_image = lib.network_predict_image
+predict_image.argtypes = [c_void_p, IMAGE]
+predict_image.restype = POINTER(c_float)
+
+network_detect = lib.network_detect
+network_detect.argtypes = [c_void_p, IMAGE, c_float, c_float, c_float, POINTER(BOX), POINTER(POINTER(c_float))]
+
+libnp = CDLL("/usr/lib/libdarknet_numpy.so", RTLD_GLOBAL)
+ndarray_image = libnp.ndarray_to_image
+ndarray_image.argtypes = [POINTER(c_ubyte), POINTER(c_long), POINTER(c_long)]
+ndarray_image.restype = IMAGE
+
+
+def detect_np(net, meta, np_img, thresh=.5, hier_thresh=.5, nms=.45):
+    im = nparray_to_image(np_img)
+    boxes = make_boxes(net)
+    probs = make_probs(net)
+    num = num_boxes(net)
+    t_start = time.time()
+    network_detect(net, im, thresh, hier_thresh, nms, boxes, probs)
+    t_end = time.time()
+    logger.debug('inference time: {} s'.format(t_end - t_start))
+    res = []
+    for j in range(num):
+        for i in range(meta.classes):
+            if probs[j][i] > 0:
+                res.append(
+                    {
+                        'type': 'detection',
+                        'label': meta.names[i].decode('utf-8'),
+                        'confidence': probs[j][i],
+                        'left': boxes[j].x,
+                        'top': boxes[j].y,
+                        'right': boxes[j].x + boxes[j].w,
+                        'bottom': boxes[j].y + boxes[j].h,
+                        'id': -1
+                    }
+                )
+    free_image(im)
+    free_ptrs(cast(probs, POINTER(c_void_p)), num)
+    return res
+
+
+def nparray_to_image(img):
+    data = img.ctypes.data_as(POINTER(c_ubyte))
+    image = ndarray_image(data, img.ctypes.shape, img.ctypes.strides)
+    return image
+
+
+class DarknetEngine(DLEngine):
+    # FIXME: Get model information by model manager
+    def __init__(self, config, model, meta='', top_k=5):
+        super(DarknetEngine, self).__init__()
+
+        self.net = load_net(config, model, 0)
+        self.meta = load_meta(meta)
+
+        # Warmup
+        zero_image = np.zeros(shape=(416, 416, 3), dtype=np.uint8)
+        detect_np(self.net, self.meta, zero_image)
+
+    def process_input(self, rgb_array):
+        return rgb_array
+
+    def inference(self, tensor):
+        return detect_np(self.net, self.meta, tensor)
+
+    def process_output(self, output):
+        return {'annotations': output}
+
+
+if __name__ == '__main__':
+    engine = DarknetEngine(
+        config=b'/usr/share/dlmodels/tinyyolovoc-20170816/tiny-yolo-voc.cfg',
+        model=b'/usr/share/dlmodels/tinyyolovoc-20170816/tiny-yolo-voc.weights',
+        meta=b'/usr/share/dlmodels/tinyyolovoc-20170816/voc.data'
+    )
+    im = cv2.imread('data/dog.jpg')
+    for i in range(3):
+        r = engine.inference(im)
+        print(r)
---
+++ berrynet/service/darknet_service.py
@@ -0,0 +1,102 @@
+# Copyright 2018 DT42
+#
+# This file is part of BerryNet.
+#
+# BerryNet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BerryNet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BerryNet.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Engine service is a bridge between incoming data and inference engine.
+"""
+
+import argparse
+import logging
+
+from berrynet import logger
+from berrynet.comm import payload
+from berrynet.dlmodelmgr import DLModelManager
+from berrynet.engine.darknet_engine import DarknetEngine
+from berrynet.service import EngineService
+
+
+class DarknetService(EngineService):
+    def __init__(self, service_name, engine, comm_config):
+        super(DarknetService, self).__init__(service_name,
+                                                engine,
+                                                comm_config)
+
+    def inference(self, pl):
+        jpg_json = payload.deserialize_payload(pl.decode('utf-8'))
+        jpg_bytes = payload.destringify_jpg(jpg_json['bytes'])
+
+        bgr_array = payload.jpg2bgr(jpg_bytes)
+
+        image_data = self.engine.process_input(bgr_array)
+        output = self.engine.inference(image_data)
+        model_outputs = self.engine.process_output(output)
+
+        self.result_hook(self.generalize_result(jpg_json, model_outputs))
+
+    def result_hook(self, generalized_result):
+        logger.debug('result_hook, annotations: {}'.format(generalized_result['annotations']))
+        self.comm.send('berrynet/engine/darknet/result',
+                       payload.serialize_payload(generalized_result))
+
+
+def parse_args():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--model',
+                    help='Model file path')
+    ap.add_argument('--label',
+                    help='Label file path')
+    ap.add_argument('--model_package',
+                    default='',
+                    help='Model package name')
+    ap.add_argument('--service_name', required=True,
+                    help='Engine service name used as PID filename')
+    ap.add_argument('--num_top_predictions', default=5,
+                    help='Display this many predictions')
+    ap.add_argument('--debug',
+                    action='store_true',
+                    help='Debug mode toggle')
+    return vars(ap.parse_args())
+
+
+def main():
+    args = parse_args()
+    if args['debug']:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    #logger.debug('model filepath: ' + args['model'])
+    #logger.debug('label filepath: ' + args['label'])
+
+    engine = DarknetEngine(
+        config=b'/usr/share/dlmodels/tinyyolovoc-20170816/tiny-yolo-voc.cfg',
+        model=b'/usr/share/dlmodels/tinyyolovoc-20170816/tiny-yolo-voc.weights',
+        meta=b'/usr/share/dlmodels/tinyyolovoc-20170816/voc.data'
+    )
+    comm_config = {
+        'subscribe': {},
+        'broker': {
+            'address': 'localhost',
+            'port': 1883
+        }
+    }
+    engine_service = DarknetService(args['service_name'],
+                                       engine,
+                                       comm_config)
+    engine_service.run(args)
+
+
+if __name__ == '__main__':
+    main()
