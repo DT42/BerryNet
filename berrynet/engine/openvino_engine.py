da0679c617281d0a37b555012cd76fb7e4bf8c35
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2019-05-12 13:21:34 +0800

    New upstream version 3.4.1

--- berrynet/engine/openvino_engine.py
+++ berrynet/engine/openvino_engine.py
@@ -142,6 +142,142 @@ class OpenVINOClassifierEngine(DLEngine):
         return {'annotations': annotations}
 
 
+class OpenVINODetectorEngine(DLEngine):
+    def __init__(self, model, device='CPU', labels=None, threshold=0.3):
+        super(OpenVINODetectorEngine, self).__init__()
+
+        # Prepare model and labels
+        model_xml = model
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
+        if labels:
+            with open(labels, 'r') as f:
+                self.labels_map = [x.strip() for x in f]
+        else:
+            self.labels_map = None
+        self.threshold = threshold
+
+        # Plugin initialization for specified device and
+        # load extensions library if specified
+        #
+        # Note: MKLDNN CPU-targeted custom layer support is not included
+        #       because we do not use it yet.
+        self.plugin = IEPlugin(device=device, plugin_dirs=None)
+
+        # Read IR
+        logger.debug("Loading network files:\n\t{}\n\t{}".format(model_xml, model_bin))
+        net = IENetwork(model=model_xml, weights=model_bin)
+
+        if self.plugin.device == "CPU":
+            supported_layers = self.plugin.get_supported_layers(net)
+            not_supported_layers = [l for l in net.layers.keys() if l not in supported_layers]
+            if len(not_supported_layers) != 0:
+                log.error("Following layers are not supported by the plugin for specified device {}:\n {}".
+                          format(self.plugin.device, ', '.join(not_supported_layers)))
+                log.error("Please try to specify cpu extensions library path in demo's command line parameters using -l "
+                          "or --cpu_extension command line argument")
+                sys.exit(1)
+
+        assert len(net.inputs.keys()) == 1, "Demo supports only single input topologies"
+        assert len(net.outputs) == 1, "Demo supports only single output topologies"
+
+        # input_blob and and out_blob are the layer names in string format.
+        logger.debug("Preparing input blobs")
+        self.input_blob = next(iter(net.inputs))
+        self.out_blob = next(iter(net.outputs))
+
+        self.n, self.c, self.h, self.w = net.inputs[self.input_blob].shape
+
+        # Loading model to the plugin
+        self.exec_net = self.plugin.load(network=net, num_requests=2)
+
+        del net
+
+        # Initialize engine mode: sync or async
+        #
+        # FIXME: async mode does not work currently.
+        #        process_input needs to provide two input tensors for async.
+        self.is_async_mode = False
+        self.cur_request_id = 0
+        self.next_request_id = 1
+
+    def __delete__(self, instance):
+        del self.exec_net
+        del self.plugin
+
+    def process_input(self, tensor, next_tensor=None):
+        frame = tensor
+        next_frame = next_tensor
+
+        # original input shape will be used in process_output
+        self.img_w = tensor.shape[1]
+        self.img_h = tensor.shape[0]
+
+        # Main sync point:
+        # in the truly Async mode we start the NEXT infer request, while waiting for the CURRENT to complete
+        # in the regular mode we start the CURRENT request and immediately wait for it's completion
+        if self.is_async_mode:
+            in_frame = cv2.resize(next_frame, (self.w, self.h))
+            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+            in_frame = in_frame.reshape((self.n, self.c, self.h, self.w))
+        else:
+            in_frame = cv2.resize(frame, (self.w, self.h))
+            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
+            in_frame = in_frame.reshape((self.n, self.c, self.h, self.w))
+        return in_frame
+
+    def inference(self, tensor):
+        inf_start = time()
+        if self.is_async_mode:
+            self.exec_net.start_async(request_id=self.next_request_id, inputs={self.input_blob: tensor})
+        else:
+            self.exec_net.start_async(request_id=self.cur_request_id, inputs={self.input_blob: tensor})
+
+        if self.exec_net.requests[self.cur_request_id].wait(-1) == 0:
+            inf_end = time()
+            det_time = inf_end - inf_start
+            if self.is_async_mode:
+                logger.debug('Inference time: N\A for async mode')
+            else:
+                logger.debug("Inference time: {:.3f} ms".format(det_time * 1000))
+
+            # Parse detection results of the current request
+            res = self.exec_net.requests[self.cur_request_id].outputs[self.out_blob]
+        else:
+            res = None
+
+        return res
+
+        # FIXME: async mode does not work currently.
+        #        process_input needs to provide two input tensors for async.
+        if self.is_async_mode:
+            self.cur_request_id, self.next_request_id = self.next_request_id, self.cur_request_id
+            frame = next_frame
+
+    def process_output(self, output):
+        logger.debug("Processing output blob")
+        logger.debug("Threshold: {}".format(self.threshold))
+
+        annotations = []
+        for obj in output[0][0]:
+            # Collect objects when probability more than specified threshold
+            if obj[2] > self.threshold:
+                xmin = int(obj[3] * self.img_w)
+                ymin = int(obj[4] * self.img_h)
+                xmax = int(obj[5] * self.img_w)
+                ymax = int(obj[6] * self.img_h)
+                class_id = int(obj[1])
+                det_label = self.labels_map[class_id] if self.labels_map else str(class_id)
+                annotations.append({
+                    'label': det_label,
+                    'confidence': float(obj[2]),
+                    'left': xmin,
+                    'top': ymin,
+                    'right': xmax,
+                    'bottom': ymax
+                })
+        return {'annotations': annotations}
+
+
 def get_distribution_info():
     """Get Debuan or Ubuntu distribution information.
     """
@@ -212,6 +348,13 @@ def set_openvino_environment():
 def parse_argsr():
     parser = ArgumentParser()
     parser.add_argument(
+            "-e", "--engine",
+            help=("Classifier or Detector engine. "
+                  "classifier, or detector is acceptable. "
+                  "(classifier by default)"),
+            default="classifier",
+            type=str)
+    parser.add_argument(
             "-m", "--model",
             help="Path to an .xml file with a trained model.",
             required=True,
@@ -252,11 +395,20 @@ def main():
     else:
         logger.setLevel(logging.INFO)
 
-    engine = OpenVINOClassifierEngine(
-                 model = args.model,
-                 device = 'CPU',
-                 labels = args.labels,
-                 top_k = args.number_top)
+    if args.engine == 'classifier':
+        engine = OpenVINOClassifierEngine(
+                     model = args.model,
+                     device = args.device,
+                     labels = args.labels,
+                     top_k = args.number_top)
+    elif args.engine == 'detector':
+        engine = OpenVINODetectorEngine(
+                     model = args.model,
+                     device = args.device,
+                     labels = args.labels)
+    else:
+        raise Exception('Illegal engine {}, it should be '
+                        'classifier or detector'.format(args.engine))
 
     #set_openvino_environment()
     #if args.debug:
