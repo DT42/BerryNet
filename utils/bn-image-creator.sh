ef5329cddd550426de6fd552e19f6c2dad381789
Bofu Chen (bafu) <bofu@dt42.io>
2019-05-23 18:44:31 +0800

    Update image creator.
    Signed-off-by: Bofu Chen (bafu) <bofu@dt42.io>

--- utils/bn-image-creator.sh
+++ utils/bn-image-creator.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
+# Copyright 2019 DT42
+#
+# This file is part of BerryNet.
+#
+# BerryNet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# BerryNet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BerryNet.  If not, see <http://www.gnu.org/licenses/>.
+
+# Generate BerryNet release image from a SD card.
+
+set -e
 
 if [ $# -lt 1 ]; then
-    echo $0 '<device>'
+    echo "Usage: $0 <device>"
     exit 0
 fi
 
@@ -20,6 +40,11 @@ CTIME_DD=`date '+%d'`
 
 IMAGE_FILENAME=$CTIME_YY-$CTIME_MM-$CTIME_DD-raspbian-stretch-berrynet
 
+echo "Creating image ${IMAGE_FILENAME}.img"
 sudo dd if="$DEVICE" of="$IMAGE_FILENAME".img bs=512
 
+echo "Compressing image to ${IMAGE_FILENAME}.zip"
 zip "$IMAGE_FILENAME".zip "$IMAGE_FILENAME".img
+
+echo "BerryNet release image has been created."
+echo "Now you can upload the image to the release repository."
