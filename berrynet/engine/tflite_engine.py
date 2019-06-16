ff2f3d00409927d2ab6243ea8fae8d2ef94b3af3
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2019-06-16 12:46:46 +0800

    tflite_engine.py: Add classification
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

--- berrynet/engine/tflite_engine.py
+++ berrynet/engine/tflite_engine.py
@@ -96,6 +96,85 @@ class TFLiteDetectorEngine(DLEngine):
             labels = list(map(str.strip, f.readlines()))
         return labels
 
+class TFLiteClassifierEngine(DLEngine):
+    def __init__(self, model, labels, top_k=5, num_threads=1,
+                 input_mean=127.5, input_std=127.5):
+        """
+        Builds Tensorflow graph, load model and labels
+        """
+        # Load labels
+        self.labels = self._load_label(labels)
+        self.classes = len(self.labels)
+
+        # Define lite graph and Load Tensorflow Lite model into memory
+        self.interpreter = tf.contrib.lite.python.interpreter.Interpreter(
+            model_path=model)
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+        self.floating_model = False
+        if self.input_details[0]['dtype'] == np.float32:
+            self.floating_model = True
+        self.input_mean = input_mean
+        self.input_std = input_std
+        self.top_k = int(top_k)
+        self.num_threads = num_threads
+
+    def __delete__(self, instance):
+        #tf.reset_default_graph()
+        #self.sess = tf.InteractiveSession()
+        del self.interpreter
+
+    def process_input(self, tensor):
+        """Resize and normalize image for network input"""
+
+        self.img_w = tensor.shape[1]
+        self.img_h = tensor.shape[0]
+
+        frame = cv2.cvtColor(tensor, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (self.input_details[0]['shape'][2], self.input_details[0]['shape'][1]))
+        frame = np.expand_dims(frame, axis=0)
+        frame = (2.0 / 255.0) * frame - 1.0
+        frame = frame.astype('float32')
+        return frame
+
+    def inference(self, tensor):
+        self.interpreter.set_num_threads(int(self.num_threads));
+        self.interpreter.set_tensor(self.input_details[0]['index'], tensor)
+        self.interpreter.invoke()
+        output_data = self.interpreter.get_tensor(output_details[0]['index'])
+        results = np.squeeze(output_data)
+        return {
+            'scores': results,
+        }
+
+    def process_output(self, output):
+        # get results
+        scores = output['scores']
+        top_k_results = scores.argsort()[-self.top_k:][::-1]
+
+        processed_output = {'annotations': []}
+
+        for i in top_k_results:
+            human_string = self.labels[i]
+            if self.floating_model:
+                score = scores[i]
+            else:
+                score = scores[i]/255.0
+            anno = {
+                'type': 'classification',
+                'label': human_string,
+                'confidence': score
+            }
+            processed_output['annotations'].append(anno)
+
+        return processed_output
+
+    def _load_label(self, path):
+        with open(path, 'r') as f:
+            labels = list(map(str.strip, f.readlines()))
+        return labels
+
 
 def parse_argsr():
     parser = ArgumentParser()
@@ -131,13 +210,17 @@ def parse_argsr():
             default=10,
             type=int)
     parser.add_argument(
+            "-nthreads", "--number_threads",
+            help="Number of threads",
+            default=1,
+            type=int)
+    parser.add_argument(
             "--debug",
             help="Debug mode toggle",
             default=False,
             action="store_true")
 
     return parser.parse_args()
-    
 
 def main():
     # Example command
@@ -151,11 +234,11 @@ def main():
         logger.setLevel(logging.INFO)
     
     if args.engine == 'classifier':
-        #engine = TFLiteClassifierEngine(
-        #             model = args.model,
-        #             device = args.device,
-        #             labels = args.labels,
-        #             top_k = args.number_top)
+        engine = TFLiteClassifierEngine(
+                     model = args.model,
+                     labels = args.labels,
+                     top_k = args.number_top,
+                     num_threads = args.number_threads)
         import sys
         sys.exit(0)
     elif args.engine == 'detector':
