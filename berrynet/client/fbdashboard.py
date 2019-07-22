23d3d0fd1243e62e754527a41eb34c52f0ba7d84
Bofu Chen (bafu) <bofu@dt42.io>
2019-07-22 23:04:17 +0800

    Update fbdashboard: add broker port param.
    Signed-off-by: Bofu Chen (bafu) <bofu@dt42.io>

--- berrynet/client/fbdashboard.py
+++ berrynet/client/fbdashboard.py
@@ -256,6 +256,12 @@ def parse_args():
         help='MQTT broker IP.'
     )
     ap.add_argument(
+        '--broker-port',
+        default=1883,
+        type=int,
+        help='MQTT broker port.'
+    )
+    ap.add_argument(
         '--topic',
         nargs='*',
         default=['berrynet/engine/tflitedetector/result'],
@@ -312,7 +318,7 @@ def main():
         'subscribe': topic_config,
         'broker': {
             'address': args['broker_ip'],
-            'port': 1883
+            'port': args['broker_port']
         }
     }
     fbd_service = FBDashboardService(comm_config,
