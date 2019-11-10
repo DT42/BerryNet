35561778183ace47d51e9e354d24cbd5fda26fa4
Bofu Chen (bafu) <bofu@dt42.io>
2019-11-10 11:51:48 +0800

    Update Telegram client based on Pylint suggestions.
    Signed-off-by: Bofu Chen (bafu) <bofu@dt42.io>

--- berrynet/client/telegram-bot.py
+++ berrynet/client/telegram-bot.py
@@ -30,6 +30,7 @@ from berrynet import logger
 from berrynet.comm import Communicator
 from berrynet.comm import payload
 
+
 # fill telegram bot token here.
 telegramToken = "000000000:AABBCCDDEEFFAA-AABBAACCAADDAAEEFFFF"
 
@@ -37,37 +38,41 @@ if ("TELEGRAM_TOKEN" in os.environ):
     telegramToken = os.environ["TELEGRAM_TOKEN"]
 
 updater = None
+cameraHandlers = []
+
 
 def hello(update, context):
     logging.info("Hello called")
     update.message.reply_text(
         'hello, {}'.format(update.message.from_user.first_name))
 
-cameraHandlers = []
-    
+
 def camera(update, context):
     logging.info("camera called id: %s" % update.message.chat_id)
     # Register the chat-id for receiving images
     if (update.message.chat_id not in cameraHandlers):
         cameraHandlers.append (update.message.chat_id)
-    
+
+
 def on_connect(client, userdata, rc, _):
     # Subscribe bn_camera
     client.subscribe("berrynet/engine/darknet/result")
+
 
 def on_message(client, userdata, msg):
     logging.info("MQTT message Topic: %s"%msg.topic)
     msg_json = payload.deserialize_payload(msg.payload);
     rawJPG = payload.destringify_jpg(msg_json["bytes"])
     photo1 = io.BytesIO(rawJPG)
-    
+
     for u in cameraHandlers:
         if updater is None:
             continue
         logging.info("Send photo to %s"%u)
         updater.bot.send_photo(chat_id = u, photo=photo1)
         pass
-    
+
+
 # Setup logging format
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
