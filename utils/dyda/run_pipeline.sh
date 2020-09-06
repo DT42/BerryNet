67abefb110ef053de83eae40e137da0e5b81c633
Bofu Chen (bafu) <bofu@dt42.io>
2020-09-06 13:14:17 +0800

    Merge bndyda package as a module of berrynet package.
    Signed-off-by: Bofu Chen (bafu) <bofu@dt42.io>

--- utils/dyda/run_pipeline.sh
+++ utils/dyda/run_pipeline.sh
@@ -10,10 +10,8 @@
 # Verified on Dyda v1.41.0
 
 dyda_source_dir="$HOME/codes/dyda"
-berrynet_source_dir="$HOME/codes/BerryNet"
 dyda_config_system_dir="/etc/dyda/pipelines/configs"
 dyda_config_local_dir="$dyda_source_dir/dyda/pipelines/configs"
-bnpipeline_dir="$berrynet_source_dir/bndyda/bndyda"
 
 # AIKEA (TFLite)
 config="$dyda_config_system_dir/object_detection_and_tracking_aikea.config"
@@ -22,4 +20,4 @@ config="$dyda_config_system_dir/object_detection_and_tracking_aikea.config"
 #config="/home/bafu/codes/bntrainer/bntrainer/object_counting_mobilenet-ssd_cpu.config"
 
 # Main
-python3 $bnpipeline_dir/bnpipeline.py --pipeline-config $config --disable-warmup -vvv --debug
+bn_pipeline --pipeline-config $config --disable-warmup -vvv --debug
